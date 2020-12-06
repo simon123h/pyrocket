b761ee82a6b7d5cd2989727d369641351c97073f
simon123h <simon123h@users.noreply.github.com>
2020-12-06 21:25:38 +0100

    Add a flame to the rocket

--- rocket.py
+++ rocket.py
@@ -8,11 +8,12 @@ class Rocket(Poly):
 
     def __init__(self, space, x, y, w=15, h=150, mass=10):
         points = [(-w/2, -h/2), (w/2, -h/2),
-                  (w/2, h/2), (0, h/1.8), (-w/2, h/2)]
+                  (w/2, h/2), (0, h/1.5), (-w/2, h/2)]
         super().__init__(space, x, y, points, mass)
         self.space = space
         self.mass = mass
         self.edgecolor = "blue"
+        self.flame_img = pygame.image.load('res/flame.png').convert_alpha()
 
         self.w = w
         self.h = h
@@ -69,19 +70,41 @@ class Rocket(Poly):
         return -self.thrust / self.mass / self.space.gravity[1]
 
     def draw(self, screen):
+        # main body
         ps = [p.rotated(self.body.angle) +
               self.body.position for p in self.shape.get_vertices()]
         ps.append(ps[0])
         for i, p in enumerate(ps):
             ps[i] = int(p.x), int(flipy(p.y))
         pygame.draw.lines(screen, pygame.Color(self.edgecolor), False, ps, 2)
+        # fins
+        h = self.h
+        w = self.w
+        fins = [(-w/2, -h/2.1), (-w*1.4, -h/2.1), (-w/2, -h/5), (-w/2, -h/2.1)]
+        fins = [pymunk.Vec2d(p).rotated(self.body.angle) + self.body.position for p in fins]
+        for i, p in enumerate(fins):
+            fins[i] = int(p.x), int(flipy(p.y))
+        pygame.draw.lines(screen, pygame.Color(self.edgecolor), False, fins, 2)
+        fins = [(w/2, -h/2.1), (w*1.4, -h/2.1), (w/2, -h/5), (w/2, -h/2.1)]
+        fins = [pymunk.Vec2d(p).rotated(self.body.angle) + self.body.position for p in fins]
+        for i, p in enumerate(fins):
+            fins[i] = int(p.x), int(flipy(p.y))
+        pygame.draw.lines(screen, pygame.Color(self.edgecolor), False, fins, 2)
+        # flame
         if self.ignited:
             thrust_pos = self.body.position + \
                 self.thrust_origin.rotated(self.body.angle)
-            ll = 20
-            angle = -self.thrust_angle + self.body.angle - math.pi/2
-            thrust_end = thrust_pos + ll * \
-                pymunk.Vec2d(math.cos(angle), math.sin(angle))
+            ll = 0.2 * math.sqrt(self.thrust)
+            angle = -self.thrust_angle + self.body.angle
             p1 = int(thrust_pos.x), int(flipy(thrust_pos.y))
+            thrust_end = thrust_pos + ll * \
+                pymunk.Vec2d(math.sin(angle), -math.cos(angle))
             p2 = int(thrust_end.x), int(flipy(thrust_end.y))
             pygame.draw.lines(screen, pygame.Color("red"), False, [p1, p2], 2)
+            w, h = self.flame_img.get_size()
+            aux_img = pygame.Surface((w*2, h*2), pygame.SRCALPHA)
+            aux_img.blit(self.flame_img, (w/2, h))
+            aux_img = pygame.transform.rotozoom(aux_img, angle*180/math.pi, 0.01*ll)
+            aux_rect = aux_img.get_rect()
+            aux_rect.centerx, aux_rect.centery = p1
+            screen.blit(aux_img, aux_rect)
