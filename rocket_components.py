3f07425035bbe05637b1a508988b0a063a339a55
simon123h <simon123h@users.noreply.github.com>
2020-12-09 10:56:54 +0100

    Add rocket component templates

---
+++ rocket_components.py
@@ -0,0 +1,37 @@
+from objects import Poly
+
+
+class RocketPart(Poly):
+    pass
+
+
+class FuelTank(RocketPart):
+    pass
+
+
+class JeEngine(RocketPart):
+    pass
+
+
+class Booster(RocketPart):
+    pass
+
+
+class NoseCone(RocketPart):
+    pass
+
+
+class Fins(RocketPart):
+    pass
+
+
+class ReactionWheel(RocketPart):
+    pass
+
+
+class AirBrakes(RocketPart):
+    pass
+
+
+class Parachute(RocketPart):
+    pass
